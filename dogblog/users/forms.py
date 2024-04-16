f48a2922131b10a9416e13c71a182921b0d87d73
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-04-16 14:20:38 +0300

    update forms profile

--- dogblog/users/forms.py
+++ dogblog/users/forms.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
@@ -20,8 +18,6 @@ class LoginUserForm(AuthenticationForm):
 
 
 class ProfilUserForm(forms.ModelForm):
-    this_year = datetime.date.today().year
-    date_birth = forms.DateField(widget=forms.SelectDateWidget(years=tuple(range(this_year - 100, this_year - 5))))
 
     class Meta:
         model = get_user_model()
