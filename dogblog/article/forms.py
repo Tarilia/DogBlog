b787dfeefaddc2cbaafa8544fb7eaad6654b3814
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-05-28 15:16:18 +0300

    update forms, add translete

--- dogblog/article/forms.py
+++ dogblog/article/forms.py
@@ -6,4 +6,4 @@ from dogblog.article.models import Article
 class CreateArticlesForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = ["slug", "title", "content", "photo", "is_published"]
+        fields = ["slug", "title", "content", "photo", "is_published", "cat", "tags"]
