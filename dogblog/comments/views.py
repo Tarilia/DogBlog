234cbc61d9c30395eef156b7c860243d6d5f9acc
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-06-09 13:51:39 +0300

    add migrate comments

--- dogblog/comments/views.py
+++ dogblog/comments/views.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from django.utils.translation import gettext as _
 from django.views.generic import ListView
 
+from dogblog.comments.models import Comments
+
 
 class IndexCommentView(ListView):
     template_name = "article/detail.html"
