c263ca94830acd5e3cb7dd5f27497242e7160bd2
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-06-09 13:56:40 +0300

    add create commits

--- dogblog/comments/urls.py
+++ dogblog/comments/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from dogblog.comments.views import IndexCommentView
+from dogblog.comments.views import IndexCommentView, CreateCommentView
 
 urlpatterns = [
     path("", IndexCommentView.as_view(), name="index_comment"),
+    path("<int:pk>/create/", CreateCommentView.as_view(), name="create_comment"),
 ]
--- dogblog/comments/views.py
+++ dogblog/comments/views.py
@@ -3,9 +3,28 @@ from django.urls import reverse
 from django.utils.translation import gettext as _
 from django.views.generic import ListView
 
+from dogblog.article import models
+from dogblog.comments.forms import CommentsForm
 from dogblog.comments.models import Comments
+from dogblog.utils import AuthRequiredMixin
 
 
 class IndexCommentView(ListView):
     template_name = "article/detail.html"
     model = Comments
+
+
+class BaseCommentView:
+    model = Comments
+
+    def get_success_url(self):
+        article = models.Article.objects.get(pk=self.object.article.pk)
+        return reverse("detail_articles",
+                       kwargs={"slug": article.slug})
+
+
+class CreateCommentView(AuthRequiredMixin, BaseCommentView,
+                        SuccessMessageMixin, CreateView):
+    template_name = "comments/create.html"
+    form_class = CommentsForm
+    success_message = _("Comment successfully created")
