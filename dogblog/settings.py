03f16d223aceb666d396fd41cf81de312b0b103d
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-06-17 15:29:35 +0300

    update DEBUG_TOOLBAR 2

--- dogblog/settings.py
+++ dogblog/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-DEBUG_TOOLBAR_CONFIG['IS_RUNNING_TESTS'] = False
+DEBUG_TOOLBAR_CONFIG = {'IS_RUNNING_TESTS': False}
 
 ALLOWED_HOSTS = ["127.0.0.1", "webserver"]
 
